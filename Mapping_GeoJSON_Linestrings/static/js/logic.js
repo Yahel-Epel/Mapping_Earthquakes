74708a82f3acd34c5b74302dcd75982bbe69eee3
Yahel Epel <yahelepel@gmail.com>
2022-03-26 14:11:47 -0700

    Update

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -37,13 +37,26 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/torontoRoutes.json";let airportData = "https://raw.githubusercontent.com/Yahel-Epel/Mapping_Earthquakes/Mapping_Single_Points/majorAirports.json";
+let torontoData = "https://raw.githubusercontent.com/Yahel-Epel/Mapping_Earthquakes/Mapping_Single_Points/Mapping_GeoJSON_Linestrings/static/js/torontoRoutes.json";
 
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 // Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
   console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
+
+  // Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+  style: myStyle,
+  onEachFeature: function(feature,layer) {
+    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
+    + feature.properties.dst + "</h3>");
+  }
+})
+.addTo(map);
 });
 
 // Then we add our 'graymap' tile layer to the map.
